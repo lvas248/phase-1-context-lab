6b2767a1ddad149129942f6614f54e2941d94af4
Luis <lvas248@gmail.com>
2022-07-04 12:52:25 -0500

    commit tree 5b4d22183a08048e527a9af037a04ee2bb5dfc79

--- index.js
+++ index.js
@@ -21,3 +21,74 @@ const allWagesFor = function () {
     return payable
 }
 
+function createEmployeeRecord(array){
+    const obj = {
+       "firstName": array[0],
+       "familyName": array[1],
+       "title": array[2],
+       "payPerHour": array[3],
+       "timeInEvents":[],
+       "timeOutEvents": []
+   }
+   return obj
+}
+
+function createEmployeeRecords(arrayOfArrays){
+   return arrayOfArrays.map(array => createEmployeeRecord(array) )
+}
+
+function createTimeInEvent(dateStamp){
+   const date = dateStamp.split(' ')[0]
+   const hour = dateStamp.split(' ')[1]
+   //const[date, hour] = dateStamp.split(' ') -- alternate strategy
+   const inEvent = {
+       "type": "TimeIn",
+       "hour": parseInt(hour),
+       "date": date
+   }
+   this.timeInEvents.push(inEvent)
+   return this
+}
+function createTimeOutEvent(dateStamp){
+   const date = dateStamp.split(' ')[0]
+   const hour = dateStamp.split(' ')[1]
+   const outEvent = {
+       "type": "TimeOut",
+       "hour": parseInt(hour),
+       "date": date
+   }
+   this.timeOutEvents.push(outEvent)
+   return this
+}
+
+function hoursWorkedOnDate(dateStamp){
+   const [date, hour] = dateStamp.split(' ')
+   const outObj = this.timeOutEvents.find(obj => obj.date === date)
+   const inObj = this.timeInEvents.find(obj => obj.date === date)
+   return (outObj.hour - inObj.hour)/100
+}
+
+function wagesEarnedOnDate(dateStamp){
+   const date = dateStamp.split(' ')[0]
+   return hoursWorkedOnDate.call(this, dateStamp) * this.payPerHour
+}
+
+function findEmployeeByFirstName(srcArray, firstName){
+   const record = srcArray.find(record => record.firstName === firstName)
+   return record
+}
+
+function calculatePayroll(arrayOfRecords){
+   return arrayOfRecords.reduce((total, record)=>{
+       return total + allWagesFor.call(record)
+       
+   },0)
+}
+
+
+let employees = [
+  ["luis", "vasquez", "sir", 20],
+  ["sasha", "vasquez", "dog", 10],
+  ["chim", "vasquez", "cat", 15],
+]
+let me = ["luis", "vasquez", "sir", 20]
